5b30896d705e52d6beea49ce006348e6d9f05fb6
KXMN <tech@kaxamana.com.br>
2022-01-25 17:40:32 -0300

    Termux dotfiles

--- termux/sh/src.d/color.sh
+++ termux/sh/src.d/color.sh
@@ -1,11 +1,12 @@
-termux-color() {
+color() {
 	path=$HOME/.termux/colors
 	if [[ -z $1 ]] || [[ ! -f $path/$1.properties ]]; then
 		for i in $path/*; do
 			echo $(basename ${i%%.properties});
 		done
 	else
-		cp "$path/$1.properties" "$PREFIX/.termux/colors.properties"
+		cp "$path/$1.properties" "$HOME/.termux/colors.properties"
+		termux-reload-settings
 		neofetch --off --color_blocks on | tail -n 6
 	fi
 }
