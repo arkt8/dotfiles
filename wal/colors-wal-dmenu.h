073b236931de2a0c2857b1d61cb1d198f02ae231
KXMN <tech@kaxamana.com.br>
2022-01-27 14:00:08 -0300

    Ignoring some files

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ffdcb5", "#202020" },
-	[SchemeSel] = { "#ffdcb5", "#df1902" },
-	[SchemeOut] = { "#ffdcb5", "#2d4b5f" },
-};
